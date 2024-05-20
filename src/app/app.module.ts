7208aa3eab046800ae0dfc90cd1de636b47a6a99
JosephAssefa-tech <yosephassefa85@gmail.com>
2024-05-20 04:47:13 -0700

    working on dashboard responsivness

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashComponent } from './dash/dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { CardComponent } from './card/card.component';  // Correct import
+//import { NgChartsModule } from 'ng2-charts';  // Correct impo
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    DashComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgChartsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule  ,
+  ],
+  providers: [
+    provideClientHydration(),
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
